63c49ba1d57fd0904b7b4286d8fc998f13d9bae9
Fata Sefer <sefer.fata@gmail.com>
2023-05-13 19:44:24 +0200

    feat: add public $customers property and initialize it

--- src/Clockodo.php
+++ src/Clockodo.php
@@ -4,15 +4,16 @@ namespace Fs98\ClockodoClient;
 
 use Fs98\ClockodoClient\Absences\Absences;
 use Fs98\ClockodoClient\Clocks\Clocks;
+use Fs98\ClockodoClient\Customers\Customers;
 
 class Clockodo
 {
-    public $absences;
-    public $clocks;
+    public $absences, $clocks, $customers;
 
     public function __construct()
     {
         $this->absences = new Absences();
         $this->clocks = new Clocks();
+        $this->customers = new Customers();
     }
 }
