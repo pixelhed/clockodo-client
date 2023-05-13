726f8899664a34f849521b03d16a107419ce3647
Fata Sefer <sefer.fata@gmail.com>
2023-05-13 19:16:36 +0200

    feat: add public $clocks property and initialize it

--- src/Clockodo.php
+++ src/Clockodo.php
@@ -3,13 +3,16 @@
 namespace Fs98\ClockodoClient;
 
 use Fs98\ClockodoClient\Absences\Absences;
+use Fs98\ClockodoClient\Clocks\Clocks;
 
 class Clockodo
 {
     public $absences;
+    public $clocks;
 
     public function __construct()
     {
         $this->absences = new Absences();
+        $this->clocks = new Clocks();
     }
 }
