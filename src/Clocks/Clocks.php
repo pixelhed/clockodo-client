2e041d171d0758ac7fd0745bedf8de430fd378f2
Fata Sefer <sefer.fata@gmail.com>
2023-05-13 19:34:08 +0200

    feat: write a method for starting a clock

--- src/Clocks/Clocks.php
+++ src/Clocks/Clocks.php
@@ -29,4 +29,33 @@ class Clocks
                 $this->clockodoApiUrl . '/v2/clock'
             )->json();
     }
+
+    /**
+     * Start the clock
+     * 
+     * @param int $customersId ID of the corresponding customer.
+     * @param int $servicesId ID of the corresponding service.
+     * @param array $optionalParameters Additional optional parameters:
+     *        - billable: (int) Is the entry billable? If omitted, the default value of the customer or the project is used.
+     *           0: not billable
+     *           1: billable
+     *           2: already billed
+     *        - projects_id (int) ID of the corresponding project.
+     *        - text (null|string) Description text. Only in list function with enhanced list mode enabled.
+     *        - users_id (int) ID of the corresponding co-worker.
+     * 
+     * @return array
+     */
+    public function start(int $customersId, int $servicesId, $optionalParameters): array
+    {
+        return Http::withHeaders($this->clockodoHeaders)
+            ->post(
+                $this->clockodoApiUrl . '/v2/clock',
+                [
+                    'customers_id' => $customersId,
+                    'services_id' => $servicesId,
+                    ...$optionalParameters
+                ]
+            )->json();
+    }
 }
