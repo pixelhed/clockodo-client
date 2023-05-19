a9da30b9082af896db2f7faf40b97613d73c8a1c
Fata Sefer <sefer.fata@gmail.com>
2023-05-19 21:07:25 +0200

    test: write unit tests for Customers.php class methods

---
+++ tests/Unit/ClocksTest.php
@@ -0,0 +1,98 @@
+<?php
+
+use Fs98\ClockodoClient\Clocks\Clocks;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Request;
+
+uses()->group('unit', 'clocks');
+
+$clockodoHeaders = [
+    'X-Clockodo-External-Application' => null,
+    'X-ClockodoApiUser' => null,
+    'X-ClockodoApiKey' => null,
+];
+$clockodoApiUrl = 'https://my.clockodo.com/api/';
+
+it('sends a GET request to get currently running entries', function () use ($clockodoHeaders, $clockodoApiUrl) {
+    // Arrange
+    Http::fake([
+        '*' => Http::response([]),
+    ]);
+
+    $clocks = new Clocks();
+
+    // Act
+    $clocks->currentlyRunning();
+
+    // Assert
+    Http::assertSentCount(1);
+    Http::assertSent(function (Request $request) use ($clockodoApiUrl, $clockodoHeaders) {
+        $mockRequestUrl = $clockodoApiUrl . 'v2/clock';
+
+        return $request->url() == $mockRequestUrl &&
+            $request->method() === 'GET' &&
+            $request->headers($clockodoHeaders);
+    });
+});
+
+it('sends a POST request to start a clock', function () use ($clockodoHeaders, $clockodoApiUrl) {
+    // Arrange
+    $mockCustomersId = 1702926;
+    $mockServicesId = 1702927;
+    $mockOptionalParameters = [
+        'text' => '...',
+        'users_id' => 1702928
+    ];
+
+    Http::fake([
+        '*' => Http::response([]),
+    ]);
+
+    $clocks = new Clocks();
+
+    // Act
+    $clocks->start($mockCustomersId, $mockServicesId, $mockOptionalParameters);
+
+    // Assert
+    Http::assertSentCount(1);
+    Http::assertSent(function (Request $request) use ($clockodoApiUrl, $clockodoHeaders, $mockCustomersId, $mockServicesId, $mockOptionalParameters) {
+        $mockData = [
+            'customers_id' => $mockCustomersId,
+            'services_id' => $mockServicesId,
+            ...$mockOptionalParameters
+        ];
+        $mockRequestUrl = $clockodoApiUrl . 'v2/clock';
+        return $request->url() == $mockRequestUrl &&
+            $request->method() === 'POST' &&
+            $request->headers($clockodoHeaders) &&
+            $request->data() === $mockData;
+    });
+});
+
+it('sends a DELETE request to stop the clock', function () use ($clockodoHeaders, $clockodoApiUrl) {
+    // Arrange
+    $mockClockId = 1702926;
+    $mockUsersId = 1702927;
+
+    Http::fake([
+        '*' => Http::response([]),
+    ]);
+
+    $clocks = new Clocks();
+
+    // Act
+    $clocks->stop($mockClockId, $mockUsersId);
+
+    // Assert
+    Http::assertSentCount(1);
+    Http::assertSent(function (Request $request) use ($clockodoApiUrl, $clockodoHeaders, $mockUsersId, $mockClockId) {
+        $mockData = [
+            'users_id' => $mockUsersId
+        ];
+        $mockRequestUrl = $clockodoApiUrl . 'v2/clock/' . $mockClockId;
+        return $request->url() == $mockRequestUrl &&
+            $request->method() === 'DELETE' &&
+            $request->headers($clockodoHeaders) &&
+            $request->data() === $mockData;
+    });
+});
